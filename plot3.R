b9c47c8dae2be96fa8aa40ccd18254bab8ee4b33
shaikfiaz <shaikfiaz83@gmail.com>
2015-11-08 23:06:48 +0530

    PNG Plots

--- plot3.R
+++ plot3.R
@@ -7,14 +7,13 @@ HousingData$DateAndTime=paste(HousingData$Date, HousingData$Time)
 HousingData$Date=as.Date(HousingData$Date,format="%d/%m/%Y")
 
 HousingData_Sub=subset(HousingData,HousingData$Date>="2007-02-01" & HousingData$Date<="2007-02-02")
-
+par(mfrow=c(1,1))
+par(mar=c(4,4,4,1))
 plot(strptime(HousingData_Sub$DateAndTime,"%d/%m/%Y %H:%M:%S"),HousingData_Sub$Sub_metering_1,type = "n",ylab = "Energy sub metering", xlab="")
 points(strptime(HousingData_Sub$DateAndTime,"%d/%m/%Y %H:%M:%S"),HousingData_Sub$Sub_metering_1,col="black",type="l")
 points(strptime(HousingData_Sub$DateAndTime,"%d/%m/%Y %H:%M:%S"),HousingData_Sub$Sub_metering_2,col="red",type="l")
 points(strptime(HousingData_Sub$DateAndTime,"%d/%m/%Y %H:%M:%S"),HousingData_Sub$Sub_metering_3,col="blue",type="l")
-legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1),,col=c("black","red","blue"))
-
-
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1),col=c("black","red","blue"))
 
 dev.copy(png, file = "/Users/Rahela/Documents/Data Science/Data/Downloaded/plot3.png") 
 dev.off() 
